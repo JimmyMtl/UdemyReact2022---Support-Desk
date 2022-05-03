ae74e3d55e3053cfd71cd7015a5b8fbfc30447cb
Jimmy <jimmy.martel@epitech.eu>
2022-05-03 13:26:31 +0200

    120. Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('server')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+    res.status(201).json({message:'Welcome to the support desk API'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
