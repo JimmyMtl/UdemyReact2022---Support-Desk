e34a8c359ad8a5f9266596f33e32092a75404c6b
Jimmy <jimmy.martel@epitech.eu>
2022-05-03 13:54:08 +0200

    121. Add Routes & Controller

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-    res.status(201).json({message:'Welcome to the support desk API'})
+    res.status(201).json({message: 'Welcome to the support desk API'})
 })
+
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
