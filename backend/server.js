08a98840e719f0d0cffc091c07a7199b92bf3d32
Jimmy <jimmy.martel@epitech.eu>
2022-05-03 14:20:08 +0200

    123. Connect To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const app = express()
+const colors = require('colors')
 const PORT = process.env.PORT || 8000
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.status(201).json({message: 'Welcome to the support desk API'})
 })
 
-
 app.use('/api/users', require('./routes/userRoutes'))
-
-
-
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
