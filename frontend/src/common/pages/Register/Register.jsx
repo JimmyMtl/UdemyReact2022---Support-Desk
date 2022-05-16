ace9fe51cbb6d3c9d75304be889443148f4431f8
Jimmy <jimmy.martel@epitech.eu>
2022-05-16 16:28:45 +0200

    132. Register User

--- frontend/src/common/pages/Register/Register.jsx
+++ frontend/src/common/pages/Register/Register.jsx
@@ -1,8 +1,9 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {FaUser} from "react-icons/fa";
 import {toast} from "react-toastify";
 import {useSelector, useDispatch} from "react-redux";
-import {register} from "../../redux/features/auth/authSlice";
+import {register, reset} from "../../redux/features/auth/authSlice";
+import {useNavigate} from "react-router-dom";
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -18,8 +19,20 @@ const Register = () => {
     }
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-    const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+    const {user, isLoading, isSuccess, isError, message} = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+        // Redirect when logged in
+        if (isSuccess || user) {
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [isError, isSuccess, user, message]);
 
     const onSubmit = (e) => {
         e.preventDefault()
