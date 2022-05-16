1065f431347aee459cf797c83c2281aff5201e58
Jimmy <jimmy.martel@epitech.eu>
2022-05-16 10:36:57 +0200

    128. Header & Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./common/pages/Home/Home";
+import Login from "./common/pages/Login/Login";
+import Register from "./common/pages/Register/Register";
+import Header from "./common/components/Header/Header";
 
 function App() {
-  return (
-    <div >
-      Hello Fro mreat
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <div className="container">
+                    <Header/>
+                    <Routes>
+                        <Route path={'/'} element={<Home/>}/>
+                        <Route path={'/login'} element={<Login/>}/>
+                        <Route path={'/register'} element={<Register/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </>
+    );
 }
 
 export default App;
